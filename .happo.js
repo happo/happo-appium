ecf0745cf13f0b677b2b07ce329b5e41437824ff
Henric Trotzig <henric.trotzig@gmail.com>
2022-06-02 13:58:20 +0200

    Add proof-of-concept Happo test
    Requires the latest happo-e2e (currently not published).
    
    This is a javascript test demonstrating how to use happo-e2e with Appium
    doing testing on an iOS device.
    
    To run:
    
    yarn install
    yarn happo-e2e -- -- node test.js

---
+++ .happo.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+module.exports = {
+  apiKey: process.env.HAPPO_API_KEY,
+  apiSecret: process.env.HAPPO_API_SECRET,
+};
---
+++ test.js
@@ -0,0 +1,35 @@
+// javascript
+
+const wdio = require('webdriverio');
+const assert = require('assert');
+const HappoController = require('happo-e2e/controller');
+
+const opts = {
+  path: '/wd/hub',
+  port: 4723,
+  capabilities: {
+    platformName: 'iOS',
+    platformVersion: '15.5',
+    deviceName: 'iPhone 13',
+    app: '../ios-test-app/build/Release-iphonesimulator/TestApp-iphonesimulator.app',
+    automationName: 'XCUITest',
+  },
+};
+
+async function main() {
+  const happoController = new HappoController();
+  await happoController.init();
+
+  const client = await wdio.remote(opts);
+  const screenshot = await client.takeScreenshot();
+  await happoController.registerLocalSnapshot({
+    component: 'Foo',
+    variant: 'bar',
+    target: 'iPhone',
+    buffer: Buffer.from(screenshot, 'base64'),
+  });
+  await client.deleteSession();
+  await happoController.finish();
+}
+
+main();
